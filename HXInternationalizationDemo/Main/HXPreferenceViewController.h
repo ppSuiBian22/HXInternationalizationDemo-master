188dc9fea201663ac59d3c396da39b72f8a0a053
Liangsen <1096359518@qq.com>
2017-09-22 10:37:58 +0800

    国际化
    App国际化

---
+++ HXInternationalizationDemo/Main/HXPreferenceViewController.h
@@ -0,0 +1,13 @@
+//
+//  HXPreferenceViewController.h
+//  HXInternationalizationDemo https://github.com/huangxuan518/HXInternationalizationDemo
+//  博客地址 http://blog.libuqing.com/
+//  Created by 黄轩 on 16/7/29.
+//  Copyright © 2016年 黄轩. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HXPreferenceViewController : UIViewController
+
+@end
